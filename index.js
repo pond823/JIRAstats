2a314ef3271b7b5374989bccf24f8c8a3e9109ce
pond823 <sasha.bilton@buzzbomb.org>
2018-06-13 15:01:23 +0100

    Samba added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ let thundercatsTotal = 0;
 let thunderCatsBlank=0;
 let thundercatsSizedTickets =0;
 
+let sambaTotal = 0;
+let sambaBlank=0;
+let sambaTickets =0;
+
 let hlTotal = 0;
 let hlBlank=0;
 let hlSizedTickets =0;
@@ -19,12 +23,14 @@ let tlSizedTickets =0;
 let thunderCatsBugs =0;
 let hlBugs = 0;
 let tlBugs = 0;
+let sambaBugs = 0;
 csv({noheader:false})
 .fromFile(`JIRA.csv`)
 .on('json',(jsonObj)=>{
     if (jsonObj[`Priority`] == `P1` && jsonObj[`Status`] == `To Do` && jsonObj[`Reporter`]!=`Raj.Dandamudi1`) {
         tickets++;
         hasTeam = false;
+        console.log(jsonObj[`Labels1`]);
         if (lowerCase(jsonObj[`Labels1`])==`thundercats` || 
             lowerCase(jsonObj[`Labels2`])==`thundercats` || 
             lowerCase(jsonObj[`Labels3`])==`thundercats` ) {
@@ -58,12 +64,30 @@ csv({noheader:false})
             }
 
         }
-        if (hasTeam == false) {
+        if (lowerCase(jsonObj[`Labels1`])==`samba` || 
+        lowerCase(jsonObj[`Labels2`])==`samba` || 
+        lowerCase(jsonObj[`Labels3`])==`samba` ) {
+            hasTeam = true;
+            value = jsonObj[`Custom field (Story Points)`];
             
+            if (value != ``) {
+                sambaTotal += parseInt(value);
+                sambaTickets++;
+            } else {
+                sambaBlank++;
+            }
+            if (jsonObj[`Issue Type`]==`Bug`) {
+                sambaBugs++;
+            } 
+        }
+    
+        if (hasTeam == false) {
+    
             value = jsonObj[`Custom field (Story Points)`];
              if (value != ``) {
                 tlTotal += parseInt(value);
                 tlSizedTickets++;
+                console.log(jsonObj)
                 
             } else {
                 
@@ -74,8 +98,8 @@ csv({noheader:false})
             }
         }
 
-
     }
+
 })
 .on('done',() => {
         console.log(`===========================================`);    
@@ -91,14 +115,20 @@ csv({noheader:false})
         console.log(`Of which ${hlBugs} are bugs`);
         
         console.log(`-------------------------------------------`);
+        console.log(`Sized Samba tickets ${sambaTickets}`);
+        console.log(`Total Samba points ${sambaTotal}`);
+        console.log(`Unsized Samba tickets ${sambaBlank}`);
+        console.log(`Of which ${sambaBugs} are bugs`);
+
+        console.log(`-------------------------------------------`);
         console.log(`Sized teamless tickets ${tlSizedTickets}`);
         console.log(`Total teamless points ${tlTotal}`);
         console.log(`Unsized teamless tickets ${tlBlank}`);
         console.log(`Of which ${tlBugs} are bugs`);
    
         console.log(`-------------------------------------------`);
-        console.log(`Total Sized tickets ${thundercatsSizedTickets+hlSizedTickets+tlSizedTickets}`);
-        console.log(`Total points ${hlTotal+thundercatsTotal+tlTotal}`);
+        console.log(`Total Sized tickets ${thundercatsSizedTickets+hlSizedTickets+tlSizedTickets+sambaTickets}`);
+        console.log(`Total points ${hlTotal+thundercatsTotal+tlTotal+sambaTotal}`);
         console.log(`Total tickets ${tickets}`);
         console.log(`===========================================`); 
         console.log(``);
