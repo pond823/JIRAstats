abb305bc8f7f6ed9e8b3ce7f13efe54bf0ef3bae
pond823 <sasha.bilton@buzzbomb.org>
2018-07-25 17:09:37 +0100

    Fixed OR on filter values AND on filters

--- UltraJiraReport.js
+++ UltraJiraReport.js
@@ -1,32 +1,32 @@
 const lowercase = require('lower-case')
-const csv=require('csvtojson')
+const csv = require('csvtojson')
 const optionDefinitions = [
-    { name: 'file', alias: 'f', type: String, defaultValue:'JIRA.csv' },
-    { name: 'verbose', alias: 'v', type: Boolean, defaultValue:false},
-    
-  ]
+    { name: 'file', alias: 'f', type: String, defaultValue: 'JIRA.csv' },
+    { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false },
+
+]
 const commandLineArgs = require('command-line-args')
 const firstLine = require('firstline')
 
-const options = commandLineArgs(optionDefinitions)  
+const options = commandLineArgs(optionDefinitions)
 
 var config = require('config');
 
 var reports = config.get("reports");
-reports.forEach(function(element) {
+reports.forEach(function (element) {
 
     element.itemCount = 0;
     element.pointsCount = 0;
     element.itemsWithPoints = 0;
-    verboseLog (element)
+    verboseLog(element)
 });
 
-if (options.verbose) { 
+if (options.verbose) {
     console.log("Options used-")
     console.log(options)
 }
 //re-write headers that repeat the same name e.g. Label, Label becomes Label, Label1
-firstLine(options.file, { lineEnding: '\n' }).then((line) => { 
+firstLine(options.file, { lineEnding: '\n' }).then((line) => {
     var headers = line.split(`,`)
     var previous = ``
     var count = 1
@@ -42,7 +42,7 @@ firstLine(options.file, { lineEnding: '\n' }).then((line) => {
         }
     }
 
-    if (options.verbose) { 
+    if (options.verbose) {
         console.log("Headers used-")
         console.log(headers)
     }
@@ -51,76 +51,63 @@ firstLine(options.file, { lineEnding: '\n' }).then((line) => {
 
 function extract(headersArray) {
 
-csv({noheader:false, headers:headersArray})
-.fromFile(options.file)
-.on('json',(jsonObj)=>{
+    csv({ noheader: false, headers: headersArray })
+        .fromFile(options.file)
+        .on('json', (jsonObj) => {
 
-    if (options.verbose) { 
-        console.log("Processing "+jsonObj['Issue key']+"\r")
+            if (options.verbose) {
+                console.log("Processing " + jsonObj['Issue key'] + "\r")
 
-    }
+            }
 
-    process (jsonObj)
+            process(jsonObj)
 
-})
-.on('done',() => {
-    reports.forEach(function(element) {
-        console.log();
-        console.log(element.title);
-        console.log(`Tickets : ${element.itemCount} Story Points ${element.pointsCount} Tickets With Points ${element.itemsWithPoints} `)  
-        console.log();
-        }
-    )
-    
-})
+        })
+        .on('done', () => {
+            reports.forEach(function (element) {
+                console.log();
+                console.log(element.title);
+                console.log(`Tickets : ${element.itemCount} Story Points ${element.pointsCount} Tickets With Points ${element.itemsWithPoints} `)
+                console.log();
+            }
+            )
+
+        })
 }
 
 
 
 function process(data) {
     verboseLog(data)
-    
-    reports.forEach(function(element) {
 
-        verboseLog(`Report - ${element.title}`)
-        var filter = element.filter;
-        match = true;
-        if (filter != undefined) {
-            filter.forEach(function(filterElement) {
-                     var filterValues = element[filterElement];
-                   
-                    if (Array.isArray(filterValues)) {
-                        
-                        filterValues.forEach(function(value) {
-                            if (lowercase(data[filterElement]) != lowercase(value)) {
-                                match = false;
-                                verboseLog(`---- ${lowercase(data[filterElement])} != ${lowercase(value)}`)
-                            } else {
-                                verboseLog(`++++ ${lowercase(data[filterElement])} == ${lowercase(value)}`)
-                            }
-                        });
-                    }
-                });
+    reports.forEach(function (report) {
+        match = false
+
+        verboseLog(`Report - ${report.title}`)
+        var filterOn = report.filterOn;
+
+        if (filterOn != undefined) {
+            match = matchAllFilterCatogories(report, data);
+        }
+
+        if (match) {
+            if (options.verbose) {
+                console.log(`FOUND ${data[`Summary`]}`)
             }
 
-            if (match) {
-                            if (options.verbose) {
-                                console.log(`FOUND ${data[`Summary`]}`)
-                            }
-           
-                            element.itemCount++;
-                            value = data[`Custom field (Story Points)`]
-                            if (value != ``) {
-                                element.pointsCount += parseInt(value);
-                                element.itemsWithPoints++;
-                                if (options.verbose) {
-                                    console.log(`SP : ${value}`)
-                                }
-                                
-                            }
+            report.itemCount++;
+            value = data[`Custom field (Story Points)`]
+            if (value != ``) {
+                report.pointsCount += parseInt(value);
+                report.itemsWithPoints++;
+                if (options.verbose) {
+                    console.log(`SP : ${value}`)
+                }
 
-            }        
-     });
+            }
+
+        }
+    });
 
 }
 
@@ -129,4 +116,36 @@ function verboseLog(msg) {
         console.log(msg);
     }
 }
+
+function matchAllFilterCatogories(report, data) {
+    var filterOn = report.filterOn;
+    var match = true
+    filterOn.forEach(function (filterElement) {
+        var filterValues = report[filterElement];
+        if (matchAnyFilterValue(filterElement, filterValues, data) != true) {
+            match = false;
+        }
+    });
+    return match
+}
+
+function matchAnyFilterValue(filterElement, filterValues, data) {
+    var match = false
+    if (Array.isArray(filterValues)) {
+
+        filterValues.forEach(function (value) {
+
+            if (lowercase(data[filterElement]) == lowercase(value)) {
+                match = true;
+                verboseLog(`++++ ${lowercase(data[filterElement])} == ${lowercase(value)}`)
+            } else {
+                verboseLog(`---- ${lowercase(data[filterElement])} != ${lowercase(value)}`)
+            }
+        });
+
+        verboseLog(`Match ${match}`)
+    }
+    return match
+}
+
 
